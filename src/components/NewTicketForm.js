6da80dcdfa84eab59287103b2e23c916d7b000b6
Jonathan Cheng <jcheng461@gmail.com>
2022-12-23 22:39:58 +0900

    add PropTypes to all components taking in props

--- src/components/NewTicketForm.js
+++ src/components/NewTicketForm.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReusableForm from "./ReusableForm";
 import { v4 } from "uuid";
+import PropTypes from "prop-types";
 
 function NewTicketForm(props) {
   function handleNewTicketFormSubmission(event) {
@@ -21,5 +22,9 @@ function NewTicketForm(props) {
     </React.Fragment>
   );
 }
+
+NewTicketForm.propTypes = {
+  onNewTicketCreation: PropTypes.func,
+};
 
 export default NewTicketForm;
